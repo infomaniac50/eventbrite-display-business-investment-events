d212fde1b14f39207ed630c8780790a0e18bf590
Derek Chafin <infomaniac50@gmail.com>
2018-10-10 03:17:28 -0500

    Filter query arguments to include only the Business & Professional/Investment category.

--- eventbrite-api-display-private-events.php
+++ eventbrite-api-display-private-events.php
@@ -1,22 +1,22 @@
 <?php
 /*
-Plugin Name: Eventbrite API - Display Private Events
-Plugin URI: https://github.com/Automattic/eventbrite-api-display-private-events
-Description: Allow the Eventbrite API plugin to display private events.
+Plugin Name: Eventbrite API - Business & Professional/Investment Only
+Plugin URI: https://github.com/infomaniac50/eventbrite-display-business-investment-events
+Description: Only show events that are in the Business & Professional/Investment category.
 Version: 1.0
-Author: Automattic
+Author: Automattic,infomaniac50
 Author URI: http://automattic.com
 License: GPL v2 or newer <https://www.gnu.org/licenses/gpl.txt>
 */
 
 /**
- * Allow the Eventbrite API plugin to display private events on eventbrite.com.
+ * Only show events that are in the Business & Professional/Investment category.
  */
-function eventbrite_display_private_events( $args ) {
-	// Enable private events.
-	$args['display_private'] = true;
+function eventbrite_display_business_investment_events( $args ) {
+  $args['category_id'] = 101; // Business & Professional
+  $args['subcategory_id'] = 1011; // Investment
 
-	// Return filtered args.
-	return $args;
+  // Return filtered args.
+  return $args;
 }
-add_filter( 'eventbrite_query_args', 'eventbrite_display_private_events' );
+add_filter( 'eventbrite_query_args', 'eventbrite_display_business_investment_events' );
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 === Eventbrite API ===
-Contributors: kwight
+Contributors: kwight,infomaniac50
 Tags: eventbrite, events, api, WordPress.com
 Requires at least: 3.8
 Tested up to: 4.1
@@ -7,4 +7,4 @@ Stable tag: 1.0.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Allow the Eventbrite API plugin to display private events from eventbrite.com.
+Only show events that are in the Business & Professional/Investment category.
